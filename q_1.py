f333fc2d22caa38148f1be2ed6bac529ecc77d67
Mona Lisa <sania5352@gmail.com>
2024-03-17 11:05:58 +0200

    my_try_2

--- q_1.py
+++ q_1.py
@@ -1,22 +1,22 @@
 def total_salary_inf(path):
-    with open('info.txt', 'r') as file:
+    sum_salary = 0
+    all_workers = 0
+
+    with open(path, 'r') as file:
         text = file.readlines()
-        workers, salary = [line.strip().split(',') for line in text]
-        sum_salary += int(salary)
-        all_workers += 1
+        for line in text:
+            name, salary = line.strip().split(',')
+            sum_salary += int(salary)
+            all_workers += 1
+
     if all_workers == 0:
-        return print("немає інформіції по робітникам")
+        return "немає інформіції по робітникам"
     else:
         average_salary = sum_salary / all_workers
-    return total_salary, average_salary
+
+    return sum_salary, average_salary
+
 
 total_salary, average_salary = total_salary_inf("path/to/info.txt")
 print(f"Загальна сума заробітної плати: {total_salary}, Середня заробітна плата: {average_salary}")
 
-
-
-
-
-
-# total, average = total_salary("path/to/info.txt")
-# print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
